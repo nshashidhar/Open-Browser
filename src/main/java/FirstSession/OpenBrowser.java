8778d5d8ab80dd15103023b79fc860a386da296a
“nshashidhar” <nshashidhar18@gmail.com>
2020-05-08 17:09:30 +0100

    hi

---
+++ src/main/java/FirstSession/OpenBrowser.java
@@ -0,0 +1,23 @@
+package FirstSession;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpenBrowser {
+	
+	
+	static WebDriver driver;
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		
+		driver = new ChromeDriver();
+		
+		driver.get("https://www.facebook.com");
+
+	}
+
+}
